d50c31cc5dbf54b53203296c77851e692f15cf5a
Akito UEHARA <akt.uehr@gmail.com>
2021-03-15 17:30:41 +0900

    numberOfStepsを返却するように

--- src/hooks/usePedometer.ts
+++ src/hooks/usePedometer.ts
@@ -1,11 +1,13 @@
 import { IPedometerData, Pedometer } from '@ionic-native/pedometer';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 export const usePedometer = () => {
+  const [numberOfSteps, setNumberOfSteps] = useState(0);
   useEffect(() => {
     const onSuccessUpdate = (pedometerData: IPedometerData) => {
       console.log('onSuccessUpdate');
       console.log(pedometerData);
+      setNumberOfSteps(pedometerData.numberOfSteps);
       // pedometerData.startDate; -> ms since 1970
       // pedometerData.endDate; -> ms since 1970
       // pedometerData.numberOfSteps;
@@ -15,4 +17,5 @@ export const usePedometer = () => {
     }
     Pedometer.startPedometerUpdates().subscribe(onSuccessUpdate)
   });
+  return { numberOfSteps };
 }
